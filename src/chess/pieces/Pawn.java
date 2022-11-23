a5ff283b13fb0f94670057de8d0943442226cc3b
VictorRodriguesMelo <victorHugorhv@gmail.com>
2022-11-23 09:16:44 -0300

    And Pawm

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -15,6 +15,7 @@ public class Pawn extends ChessPiece{
 	public boolean[][] possibleMoves() {
 		boolean [][] mat = new boolean [getBoard().getRows()][getBoard().getColumns()];
 		Position p = new Position(0,0);
+		
 		if (getColor() == Color.WHITE) {
 			p.setValues(position.getRow() -1 , position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
@@ -59,7 +60,7 @@ public class Pawn extends ChessPiece{
 	}	
 		@Override
 		public String toString() {
-			return "p";
+			return "P";
 		}
 
 }
